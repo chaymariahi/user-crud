5d5d9533d2e86864084c46c55dc05564292cd861
chayma <chaymariahi872@gmail.com>
2023-09-04 00:00:52 +0100

    updates

--- src/test/java/com/stage/userCrud/model/UserTest.java
+++ src/test/java/com/stage/userCrud/model/UserTest.java
@@ -6,7 +6,7 @@ import static org.junit.jupiter.api.Assertions.assertNull;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-public class UserTest {
+class UserTest {
     private User user;
 
     @BeforeEach
@@ -15,78 +15,78 @@ public class UserTest {
     }
 
     @Test
-    public void testGetAge() {
+    void testGetAge() {
         assertEquals(25, user.getAge());
     }
 
     @Test
-    public void testSetAge() {
+    void testSetAge() {
         user.setAge(30);
         assertEquals(30, user.getAge());
     }
 
     @Test
-    public void testGetFirstName() {
+    void testGetFirstName() {
         assertEquals("John", user.getFirstName());
     }
 
     @Test
-    public void testSetFirstName() {
+    void testSetFirstName() {
         user.setFirstName("Jane");
         assertEquals("Jane", user.getFirstName());
     }
 
     @Test
-    public void testGetLastName() {
+    void testGetLastName() {
         assertEquals("Doe", user.getLastName());
     }
 
     @Test
-    public void testSetLastName() {
+    void testSetLastName() {
         user.setLastName("Smith");
         assertEquals("Smith", user.getLastName());
     }
 
     @Test
-    public void testGetPhoneNumber() {
+    void testGetPhoneNumber() {
         assertEquals(34567890, user.getPhoneNumber());
     }
 
     @Test
-    public void testSetPhoneNumber() {
+    void testSetPhoneNumber() {
         user.setPhoneNumber(98765432);
         assertEquals(98765432, user.getPhoneNumber());
     }
 
     @Test
-    public void testGetGender() {
+    void testGetGender() {
         assertEquals("Male", user.getGender());
     }
 
     @Test
-    public void testSetGender() {
+    void testSetGender() {
         user.setGender("Female");
         assertEquals("Female", user.getGender());
     }
 
     @Test
-    public void testGetAddress() {
+    void testGetAddress() {
         assertEquals("123 Main St", user.getAddress());
     }
 
     @Test
-    public void testSetAddress() {
+    void testSetAddress() {
         user.setAddress("456 Elm St");
         assertEquals("456 Elm St", user.getAddress());
     }
 
     @Test
-    public void testGetOccupation() {
+    void testGetOccupation() {
         assertEquals("Engineer", user.getOccupation());
     }
 
     @Test
-    public void testSetOccupation() {
+    void testSetOccupation() {
         user.setOccupation("Teacher");
         assertEquals("Teacher", user.getOccupation());
     }
